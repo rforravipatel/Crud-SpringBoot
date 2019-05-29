b471e27a21dd1f441c1285b15c4f926628297981
Ravi <tempravi789@gmail.com>
2019-05-29 10:17:08 -0400

    CORS add

--- src/main/java/com/learning/CrudSpringBoot/rest/EmployeeRestController.java
+++ src/main/java/com/learning/CrudSpringBoot/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ public class EmployeeRestController {
 	}
 
 	@GetMapping("/employees")
+	@CrossOrigin(origins = "http://localhost:8070")
 	public List<Employee> findAll() {
 		return employeeService.findAll();
 	}
